2eb4cd8d8bd8ffa406a9d53cce2ea0dee9a3df69
umerk1010 <145277540+umerk1010@users.noreply.github.com>
2024-07-29 19:57:44 +0530

    Add files via upload

---
+++ scientific calculator/script.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const display = document.getElementById('screen');
+    const buttons = document.getElementsByClassName('btn');
+  
+    let currentValue = "";
+  
+    function evaluateResult() {
+      console.log('currentValue:', currentValue)
+      const convertedValue = currentValue
+        .replace("×", "*")
+        .replace("÷", "/")
+        .replace('%', '*0.01')
+        .replace('sin', 'Math.sin')
+        .replace('cos', 'Math.cos')
+        .replace('ln', 'Math.log')
+        .replace('π', 'Math.PI')
+        .replace('log', 'Math.log10')
+        .replace('e', 'Math.E')
+        .replace('tan', 'Math.tan')
+        .replace('√', 'Math.sqrt');
+      
+      console.log('convertedValue:', convertedValue)
+      const result = eval(convertedValue);
+      currentValue = result.toString();
+      display.value = currentValue;
+    }
+  
+    for (let i = 0; i < buttons.length; i++) {
+      const button = buttons[i];
+      button.addEventListener('click', function() {
+        const value = button.innerText;
+
+        try {
+            if (value == "AC") {
+                currentValue = "";
+                display.value = currentValue;
+              } else if(value == "=") {
+                evaluateResult();
+              } else {
+                currentValue += value;
+                display.value = currentValue;
+              }
+        } catch (error) {
+            console.error(error);
+            currentValue = "ERROR";
+            display.value = currentValue;
+        }
+
+        
+      })
+      
+    }
+  
+  
+  });
